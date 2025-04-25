f88eb88df3c6d661e2c8778a65e05f511682bdd8
jason13657 <jong13657@gmail.com>
2025-04-25 12:55:10 +1000

    user api

--- src/main/java/dao/UserDAOImpl.java
+++ src/main/java/dao/UserDAOImpl.java
@@ -8,12 +8,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import dao.interfaces.UserDAO;
 import model.User;
 
-public class UserDAO {
+public class UserDAOImpl implements UserDAO{
     private final Connection connection;
 
-    public UserDAO(Connection connection) {
+    public UserDAOImpl(Connection connection) {
         this.connection = connection;
     }
 
@@ -90,8 +92,37 @@ public class UserDAO {
         return null;
     }
 
+    // READ BY EMAIL
+    public List<User> getUsersByEmail(String email) throws SQLException {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users WHERE email Like ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    User user = new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("email"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("password"),
+                        resultSet.getString("gender"),
+                        resultSet.getString("favorite_color"),
+                        resultSet.getObject("date_of_birth", LocalDate.class),
+                        resultSet.getObject("created_at", LocalDateTime.class),
+                        resultSet.getObject("updated_at", LocalDateTime.class),
+                        resultSet.getString("role"),
+                        resultSet.getBoolean("is_active")
+                    );
+                users.add(user);
+            }
+        }
+        return users;
+        }
+    }
+
     // UPDATE
-    public void updateUser(User user) throws SQLException {
+    public void updateUser(int id, User user) throws SQLException {
         String query = "UPDATE users SET email = ?, first_name = ?, last_name = ?, password = ?, gender = ?, favorite_color = ?, date_of_birth = ?, created_at = ?, updated_at = ?, role = ?, is_active = ? WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, user.getEmail());
@@ -105,7 +136,7 @@ public class UserDAO {
             statement.setObject(9, user.getUpdatedAt());
             statement.setString(10, user.getRole());
             statement.setBoolean(11, user.isActive());
-            statement.setInt(12, user.getId());
+            statement.setInt(12, id);
             statement.executeUpdate();
         }
     }
