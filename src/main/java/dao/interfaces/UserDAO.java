127c67208597b6b8ce465a00806b6b48929e95f8
jason13657 <jong13657@gmail.com>
2025-04-25 12:48:22 +1000

    user controller and api

---
+++ src/main/java/dao/interfaces/UserDAO.java
@@ -0,0 +1,14 @@
+package dao.interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.User;
+
+public interface UserDAO {
+    void createUser(User user) throws SQLException;
+    User getUserById(int id) throws SQLException;
+    List<User> getAllUsers() throws SQLException;
+    List<User> getUsersByEmail(String email) throws SQLException;
+    void updateUser(int id, User user) throws SQLException;
+    void deleteUser(int id) throws SQLException;
+}
