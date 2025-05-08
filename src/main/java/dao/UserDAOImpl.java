2f10ad6019534f13449938e3b3a094a1545574df
jason13657 <jong13657@gmail.com>
2025-05-08 19:28:05 +1000

    login

--- src/main/java/dao/UserDAOImpl.java
+++ src/main/java/dao/UserDAOImpl.java
@@ -70,6 +70,20 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
+    public User getUserByEmail(String email) throws SQLException {
+    String query = "SELECT * FROM users WHERE email = ?";
+    try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setString(1, email);
+        try (ResultSet rs = statement.executeQuery()) {
+            if (rs.next()) {
+                return mapResultSetToUser(rs);
+            }
+        }
+    }
+    return null; // or throw exception if not found
+}
+
+    @Override
     public void updateUser(int id, User user) throws SQLException {
         String query = "UPDATE users SET email = ?, first_name = ?, last_name = ?, password = ?, gender = ?, favorite_color = ?, date_of_birth = ?, created_at = ?, updated_at = ?, role = ?, is_active = ? WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
--- src/main/java/dao/interfaces/UserDAO.java
+++ src/main/java/dao/interfaces/UserDAO.java
@@ -9,6 +9,7 @@ public interface UserDAO {
     User getUserById(int id) throws SQLException;
     List<User> getAllUsers() throws SQLException;
     List<User> getUsersByEmail(String email) throws SQLException;
+    User getUserByEmail(String email) throws SQLException;
     void updateUser(int id, User user) throws SQLException;
     void deleteUser(int id) throws SQLException;
 }
