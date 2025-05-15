b6c97fc0c6ea656a1e27c4b1e2b1138bea552760
jason13657 <jong13657@gmail.com>
2025-05-15 16:41:29 +1000

    Ignore compiled classes and target directory

---
+++ src/main/java/controller/MeController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import model.User;
+
+
+@WebServlet("/api/me")
+public class MeController extends HttpServlet {
+
+    private final Gson gson = new Gson();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            Object userObj = session.getAttribute("user");
+            if (userObj instanceof User) {
+                User user = (User) userObj;
+                response.getWriter().write(gson.toJson(user));
+                return;
+            }
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{\"error\": \"Not logged in\"}");
+    }
+}
